5b542a1baa9e33c28b58136e03e12cfd9e38340d
Leonard de Ruijter <leonard@babbage.com>
2017-07-21 13:21:06 +0200

    More locationUtils works

--- source/locationUtils.py
+++ source/locationUtils.py
@@ -35,12 +35,8 @@ class Point(namedtuple("Point",("x","y"))):
 			return super(Point,self).__rsub__(other)
 		return Point((self.x-other.x),(self.y-other.y))
 
-class _RectMixIn(object):
+class _RectMixin:
 	"""Mix-in class for properties shared between Location and Rect classes"""
-
-	@classmethod
-	def __new__(self, *args, **kwargs):
-		raise NotImplementedError("This mix-in class can only be used as such")
 
 	def toCRect(self):
 		return RECT(self.left,self.top,self.right,self.bottom)
@@ -61,9 +57,40 @@ class _RectMixIn(object):
 		if isinstance(other,_POINT_CLASSES):
 			return other.x >= self.left < self.right and other.y >= self.top < self.bottom
 		if isinstance(other,_RECT_CLASSES):
-			return self.left >= other.left and self.top >= other.top and self.right <= other.right and self.bottom <= other.bottom
+			return self<other
+		return super(type(self),self).__contains__(other)
 
-class Location(namedtuple("Location",("left","top","width","height")), _RectMixIn):
+	def __lt__(self, other):
+		if isinstance(other,_RECT_CLASSES):
+			return self.left > other.left and self.top > other.top and self.right < other.right and self.bottom < other.bottom
+		return super(type(self),self).__lt__(other)
+
+	def __le__(self, other):
+		if isinstance(other,_RECT_CLASSES):
+			return self.left >= other.left and self.top >= other.top and self.right <= other.right and self.bottom <= other.bottom
+		return super(type(self),self).__le__(other)
+
+	def __gt__(self, other):
+		if isinstance(other,_RECT_CLASSES):
+			return other.left > self.left and other.top > self.top and other.right < self.right and other.bottom < self.bottom
+		return super(type(self),self).__gt__(other)
+
+	def __ge__(self, other):
+		if isinstance(other,_RECT_CLASSES):
+			return other.left >= self.left and other.top >= self.top and other.right <= self.right and other.bottom <= self.bottom
+		return super(type(self),self).__ge__(other)
+
+	def __eq__(self, other):
+		if isinstance(other,_RECT_CLASSES):
+			return other.left == self.left and other.top == self.top and other.right == self.right and other.bottom == self.bottom
+		return super(type(self),self).__eq__(other)
+
+	def __neq__(self, other):
+		if isinstance(other,_RECT_CLASSES):
+			return not (other.left == self.left and other.top == self.top and other.right == self.right and other.bottom == self.bottom)
+		return super(type(self),self).__neq__(other)
+
+class Location(_RectMixin, namedtuple("Location",("left","top","width","height"))):
 	"""Represents a rectangle on the screen, based on left and top coordinates, width and height."""
 
 	@property
@@ -97,7 +124,7 @@ class Location(namedtuple("Location",("left","top","width","height")), _RectMixI
 		right,bottom=winUser.ClientToScreen(hwnd, self.right, self.bottom)
 		return Location(left,top,(right-left),(bottom-top))
 
-class Rect(namedtuple("Rect",("left","top","right","bottom")), _RectMixIn):
+class Rect(_RectMixin, namedtuple("Rect",("left","top","right","bottom"))):
 	"""Represents a rectangle on the screen.
 	By convention, the right and bottom edges of the rectangle are normally considered exclusive.
 	"""
@@ -133,47 +160,64 @@ class Rect(namedtuple("Rect",("left","top","right","bottom")), _RectMixIn):
 		right,bottom=winUser.ClientToScreen(hwnd, self.right, self.bottom)
 		return Rect(left,top,right,bottom)
 
-def toRect(param):
-	if isinstance(param,Rect):
-		return param
-	if isinstance(param,_RECT_CLASSES):
-		return Rect(param.left,param.top,param.right,param.bottom)
-	if isinstance(param,_POINT_CLASSES):
-		# Right and bottom edges of the resulting rectangle are considered exclusive
-		x,y=point.x,point.y
-		return Rect(x,y,x+1,y+1)
-	if isinstance(param,tuple):
-		if len(param)==4 and isinstance(param[0],int) and isinstance(param[1],int) and isinstance(param[2],int) and isinstance(param[3],int):
-			# Assume that we are converting from a tuple rectangle
-			return Rect(*param)
-		if len(param)==2 and isinstance(param[0],int) and isinstance(param[1],int):
-			x,y=param
+def toRect(*params):
+	if len(params)==0:
+		raise TypeError("This function takes at least 1 argument (0 given)")
+	if len(params)==1:
+		param=params[0]
+		if isinstance(param,Rect):
+			return param
+		if isinstance(param,_RECT_CLASSES):
+			return Rect(param.left,param.top,param.right,param.bottom)
+		if isinstance(param,_POINT_CLASSES):
+			# Right and bottom edges of the resulting rectangle are considered exclusive
+			x,y=point.x,point.y
 			return Rect(x,y,x+1,y+1)
-		if len(param)==2 and isinstance(param[0],_POINT_CLASSES) and isinstance(param[1],_POINT_CLASSES):
-			left,top=param[0].x,param[0].y
-			right,bottom=param[1].x,param[1].y
-			return Rect(left,top,right,bottom)
+		if isinstance(param,(tuple,list)):
+			# One indexable in another indexable doesn't make sence, so treat the inner indexable as outer indexable
+			params=param
+	if all(isinstance(param,int) for param in params):
+		if len(params)==4:
+			# Assume that we are converting from a tuple rectangle.
+			# To convert from a tuple location, use L{toLocation} instead.
+			# To convert from a tuple rectangle to L{Location}, use this function and execute L{toLocation} on the resulting object.
+			return Rect(*params)
+		elif len(param)==2:
+			x,y=params
+			return Rect(x,y,x+1,y+1)
+	xs=[]
+	ys=[]
+	for param in params:
+		if isinstance(param,_RECT_CLASSES):
+			xs.extend((param.left,param.right))
+			ys.extend((param.top,param.bottom))
+		elif isinstance(param,_POINT_CLASSES):
+			xs.append(param.x)
+			ys.append(param.y)
+		else:
+			raise ValueError("Unexpected parameter %s"%param)
+	left=min(xs)
+	top=min(ys)
+	right=max(xs)
+	bottom=max(ys)
+	return Rect(left,top,right,bottom)
 
-def toLocation(param):
-	if isinstance(param,Location):
-		return param
-	if isinstance(param,_RECT_CLASSES):
-		return Location(param.left,param.top,param.right-param.left,param.bottom-param.top)
-	if isinstance(param,_POINT_CLASSES):
-		# Right and bottom edges of the resulting rectangle are considered exclusive
-		x,y=point.x,point.y
-		return Location(x,y,1,1)
-	if isinstance(param,tuple):
-		if len(param)==4 and isinstance(param[0],int) and isinstance(param[1],int) and isinstance(param[2],int) and isinstance(param[3],int):
-			# Assume that we are converting from a tuple location
-			return Location(*param)
-		if len(param)==2 and isinstance(param[0],int) and isinstance(param[1],int):
-			x,y=param
-			return Location(x,y,1,1)
-		if len(param)==2 and isinstance(param[0],_POINT_CLASSES) and isinstance(param[1],_POINT_CLASSES):
-			left,top=param[0].x,param[0].y
-			right,bottom=param[1].x,param[1].y
-			return Location(left,top,right-left,bottom-top)
+def toLocation(*params):
+	if len(params)==0:
+		raise TypeError("This function takes at least 1 argument (0 given)")
+	if len(params)==1:
+		param=params[0]
+		if isinstance(param,Location):
+			return param
+		if isinstance(param,(tuple,list)):
+			# One indexable in another indexable doesn't make sence, so treat the inner indexable as outer indexable
+			params=param
+	if len(params)==4 and all(isinstance(param,int) for param in params):
+		# Assume that we are converting from a tuple location.
+		# To convert from a tuple rectangle, use L{toRectangle} instead.
+		# To convert from a tuple location to L{Rect}, use this function and execute L{toRect} on the resulting location.
+		return Location(*params)
+	return toRect(*params).toLocation()
 
 _POINT_CLASSES=(Point,POINT,textInfos.Point)
 _RECT_CLASSES=(Rect,Location,RECT,SMALL_RECT,textInfos.Rect)
